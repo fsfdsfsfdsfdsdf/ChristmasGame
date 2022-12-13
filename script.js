9a5a7b1c87f1da7c1335ed84275d076a9847b913
Ivan Havran <icrazy2001@gmail.com>
2022-12-14 01:06:12 +0200

    Added a pop-up for settings. Fixed main menu

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function changePage(a){
     let gp =  document.querySelector(".gamePage");
     let mp = document.querySelector(".mainPage");
     let hb = document.querySelector(".Page");
+    let ft = document.querySelector("footer");
     if(a == 1){
         gp.style.display = "flex";
         mp.style.display = "none";
         hb.style.background = "darkred";
+        ft.style.display = "none";
     }else{
         gp.style.display = "none";
         mp.style.display = "";
         hb.style.background = "";
+        ft.style.display = "";
     }
 }
 
@@ -25,6 +28,32 @@ function loginPopup() {
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
+        }
+    }
+}
+
+//Функция для показа регистрационной панели или логинной
+function showReg(a) {
+    if(a == 1){
+        document.querySelector(".divRegForm").style.display = "block";
+        document.querySelector(".divLogForm").style.display = "none";
+    }else{
+        document.querySelector(".divRegForm").style.display = "none";
+        document.querySelector(".divLogForm").style.display = "block";
+    }
+}
+
+//Функция для открытия попапа с настройками
+function settingsPopup() {
+    var settings = document.getElementById("settings");
+    var csc = document.getElementsByClassName("close_settings_content")[0];
+    settings.style.display = "block";
+    csc.onclick = function () {
+        settings.style.display = "none";
+    }
+    window.onclick = function (event) {
+        if (event.target == settings) {
+            settings.style.display = "none";
         }
     }
 }
